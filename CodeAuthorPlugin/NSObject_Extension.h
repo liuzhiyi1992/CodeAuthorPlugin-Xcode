1aff16daa410ed1fcb706ca24de958de621cd686
liuzhiyi1992 <55137840@qq.com>
2016-05-11 11:39:41 +0800

    category

--- CodeAuthorPlugin/NSObject_Extension.h
+++ CodeAuthorPlugin/NSObject_Extension.h
@@ -10,7 +10,7 @@
 
 @interface NSObject (Xcode_Plugin_Template_Extension)
 
-+ (void)pluginDidLoad:(NSBundle *)plugin;
+//+ (void)pluginDidLoad:(NSBundle *)plugin;
 
 
 @end
--- CodeAuthorPlugin/NSObject_Extension.m
+++ CodeAuthorPlugin/NSObject_Extension.m
@@ -12,14 +12,14 @@
 
 @implementation NSObject (Xcode_Plugin_Template_Extension)
 
-+ (void)pluginDidLoad:(NSBundle *)plugin
-{
-    static dispatch_once_t onceToken;
-    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
-    if ([currentApplicationName isEqual:@"Xcode"]) {
-        dispatch_once(&onceToken, ^{
-            sharedPlugin = [[CodeAuthorPlugin alloc] initWithBundle:plugin];
-        });
-    }
-}
+//+ (void)pluginDidLoad:(NSBundle *)plugin
+//{
+//    static dispatch_once_t onceToken;
+//    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+//    if ([currentApplicationName isEqual:@"Xcode"]) {
+//        dispatch_once(&onceToken, ^{
+////            sharedPlugin = [[CodeAuthorPlugin alloc] initWithBundle:plugin];
+//        });
+//    }
+//}
 @end
